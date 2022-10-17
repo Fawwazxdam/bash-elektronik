049281a791c53a18ff196a566248afdc432932ee
Adam Fawwaz <fawwazadam1005@gmail.com>
2022-10-17 19:58:41 +0700

    first commit

---
+++ database/migrations/2022_10_17_075756_create_galeris_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galeri', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produk_id')->constrained('produk')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('foto1');
+            $table->string('foto2');
+            $table->string('foto3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galeri');
+    }
+};
